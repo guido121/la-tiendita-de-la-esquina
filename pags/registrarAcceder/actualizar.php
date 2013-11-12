fb18bc3113cbd78127c4dc446239e5a648312edb
taily <taily_93@hotmail.com>
2013-11-11 23:05:29 -0500

    actualizar
    Al fin !!!!!

--- pags/registrarAcceder/actualizar.php
+++ pags/registrarAcceder/actualizar.php
@@ -1,16 +1,26 @@
+<?
+
+$ruc = $_SESSION['ruc'];
+
+$consulta = "SELECT * FROM empresa WHERE codigoEmpresa = '$ruc'";
+$datos = consulta_bd($consulta, $config);
+
+?>
+
+
 <div>
 
 <h1>Actualizar Datos</h1>
 
-<form action="procesar/procesar_edicion.php" method="post">
-	<label for="dni">
-		DNI : <?php echo  $datos[0]['DNI']; ?>
-		<input name="dni" type="hidden" value="<?php echo  $datos[0]['DNI'];?>" />
+<form action="./procesos/procesar_edicion.php" method="post">
+	<label for="ruc">
+		RUC : <?php echo  $datos[0]['ruc']; ?>
+		<input name="ruc" type="hidden" value="<?php echo  $datos[0]['ruc'];?>" />
 	</label></br>
-	<label>Nombre</label><input type="text" name="nombre" value="<? echo $datos[0]['Nombre'];?>"/></br>
-	<label>Apellido</label><input type="text" name="apellido" value="<? echo $datos[0]['Apellido'];?>"/></br>
-	<label>Edad</label><input type="text" name="edad" value="<? echo $datos[0]['Edad'];?>" />
-
+	<label>Razon Social :</label><input type="text" name="razon_social" value="<? echo $datos[0]['razonSocial'];?>"/></br>
+	<label>Direccion Fiscal :</label><input type="text" name="direc_fiscal" value="<? echo $datos[0]['direccionFiscal'];?>"/></br>
+	<label>Telefono :</label><input type="text" name="telefono" value="<? echo $datos[0]['telefono'];?>" /></br>
+	<label>Email :</label><input type="text" name="email" value="<? echo $datos[0]['correo'];?>" /></br>
 	<input type="submit" value="Guardar"/>
 </form>
 
